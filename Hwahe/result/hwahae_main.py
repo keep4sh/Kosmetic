20fbe519ad47f15de7661f24f653b3f3d975da79
Jicodeac <166669400+Jicodeac@users.noreply.github.com>
2024-08-08 10:32:45 +0900

    Hwahae crawlwer and main file in .py format

---
+++ Hwahe/result/hwahae_main.py
@@ -0,0 +1,32 @@
+from hwahae_crawler import CrawlTopN, CallProductDetail
+import pandas as pd
+
+def hwahae_main():
+    category_id = {'스킨/토너': 4156,
+    '로션/에멀젼': 4165,
+    '에센스/엠플/세럼': 4174,
+    '페이스오일': 4183,
+    '크림': 4184,
+    '아이케어': 4193,
+    '미스트': 4194,
+    '젤': 4199,
+    '스킨/토너 패드': 4498,
+    '밤/멀티밤': 4518}
+
+    frames = []
+    for category in category_id:
+        id = category_id[category]
+        type_ = CrawlTopN(category, id)
+        top100 = type_.call_topn_products(5)
+        crawl_list = [x for x in top100['id']]
+        crawler = CallProductDetail(crawl_list)
+        reviews = crawler.crawl_all()
+        final = top100.join(reviews.set_index('id'), on='id')
+        frames.append(final)
+
+    result = pd.concat(frames, ignore_index=True, sort=False)
+    result.to_csv('hwahae_data.csv')
+    print('File successfully saved!')
+
+if __name__ =='__main__':
+    hwahae_main()
